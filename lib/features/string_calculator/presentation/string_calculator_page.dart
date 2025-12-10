06a1ed6ff5428b491fdd441f89ae83a88e814afd
Shivam Verma <vermashivam1995@gmail.com>
2025-12-11 01:01:34 +0530

    Initial string calculator project setup

---
+++ lib/features/string_calculator/presentation/string_calculator_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StringCalculatorPage extends StatelessWidget {
+  const StringCalculatorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
