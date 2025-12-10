a7a22f3121ccd3a0ab5a18de6cd3a4674173e4b1
Shivam Verma <vermashivam1995@gmail.com>
2025-12-11 02:03:26 +0530

    after writing repo tests

--- test/features/string_calculator/data/string_calculator_repo_impl_test.dart
+++ test/features/string_calculator/data/string_calculator_repo_impl_test.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:string_calculator_kata/core/error/negative_number_exception.dart';
 import 'package:string_calculator_kata/features/string_calculator/data/repo_impl/string_calculator_repo_impl.dart';
 
 void main() {
@@ -18,5 +21,31 @@ void main() {
 
   test("two numbers comma seprated are summed", () {
     expect(repo.add("1,5"), 6);
+  });
+
+  test("handles any amount of numbers", () {
+    expect(repo.add("1,2,3,4"), 10);
+  });
+
+  test("handles new lines between numbers", () {
+    expect(repo.add("1\n2,3"), 6);
+  });
+
+  test("supports custom delimiter", () {
+    expect(repo.add("//;\n1;2;3"), 6);
+  });
+
+  test("throws exception for single negative number", () {
+    expect(() => repo.add("-1"), throwsA(isA<NegativeNumberException>()));
+  });
+
+  test("exception message contains more than one negative numbers", () {
+    try {
+      repo.add('-1,2,-3');
+      fail("should have thrown NegativeNumberException");
+    } on NegativeNumberException catch (e) {
+      expect(e.numbers, [-1, -3]);
+      expect(e.toString(), "negative numbers not allowed: -1,-3");
+    }
   });
 }
